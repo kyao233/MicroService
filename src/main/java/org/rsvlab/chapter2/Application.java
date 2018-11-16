cdce617eff2cf75a4b413677aaa15dfa29b4f8de
kyao233 <kyao233@163.com>
2018-11-16 17:39:05 +0800

    add basic auth for all method

--- src/main/java/org/rsvlab/chapter2/Application.java
+++ src/main/java/org/rsvlab/chapter2/Application.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * enable auto config annotation.
  * You can also change the default config value in application.properties file.
  * */
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class Application {
 
