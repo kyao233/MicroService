189d601032c462265fb82cbec795f599b37f0d20
kyao233 <kyao233@163.com>
2018-11-17 14:40:41 +0800

    add old rest project, ++messaging between ms

---
+++ Legacyrest/src/main/java/org/rvslab/chapter2/HomeController.java
@@ -0,0 +1,61 @@
+package org.rvslab.chapter2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Handles requests for the application home page.
+ */
+@RestController
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping("/")
+	public Greet home() {
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+		
+		
+		Greet welcome = new Greet("Hello world!");
+		
+		return welcome;
+	}
+	
+	
+	class Greet {
+		
+		private String message = null;
+		public Greet(String message) {
+			this.message = message;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		
+		
+		
+		
+	}
+	
+}
