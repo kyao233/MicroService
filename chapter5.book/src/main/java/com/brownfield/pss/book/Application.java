5bc68dd08bdfb6fadb7f21ebababa255595a53aa
kyao233 <kyao233@163.com>
2019-01-25 22:32:44 +0800

    chapter5

--- chapter5.book/src/main/java/com/brownfield/pss/book/Application.java
+++ chapter5.book/src/main/java/com/brownfield/pss/book/Application.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 import com.brownfield.pss.book.component.BookingComponent;
 import com.brownfield.pss.book.entity.BookingRecord;
@@ -18,6 +19,7 @@ import com.brownfield.pss.book.entity.Inventory;
 import com.brownfield.pss.book.entity.Passenger;
 import com.brownfield.pss.book.repository.BookingRepository;
 import com.brownfield.pss.book.repository.InventoryRepository;
+
 
 @SpringBootApplication
 public class Application implements CommandLineRunner{
