82d9ed6b67277e8e0eebe8f9d6fa22a995a05ee7
kyao233 <kyao233@163.com>
2019-01-25 22:43:38 +0800

    delete sub-module

---
+++ chapter5.book/src/main/java/com/brownfield/pss/book/component/FareServiceProxy.java
@@ -0,0 +1,15 @@
+package com.brownfield.pss.book.component;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="fares-proxy", url="localhost:8080/fares")
+public interface FareServiceProxy {
+	
+	@RequestMapping(value = "/get", method=RequestMethod.GET)
+	Fare getFare(@RequestParam(value="flightNumber") String
+			flightNumber, @RequestParam(value="flightDate") String
+			flightDate);
+}
